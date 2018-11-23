990bdf2520f30d1a49336327d2bb1e25c95b6cfb
Muhammad Rohman Irfanuddin <muhammad.rohman.irfanuddin09@gmail.com>
2018-11-23 16:12:59 +0700

    publish and create jwt.php file in app directory

--- config/app.php
+++ config/app.php
@@ -176,9 +176,8 @@ return [
         Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
         Zizaco\Entrust\EntrustServiceProvider::class,
 
-        'JWTAuth'   => Tymon\JWTAuth\Facades\JWTAuth::class,
-        'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class,
-        'Entrust' => Zizaco\Entrust\EntrustServiceProvider::class
+        Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
+		Zizaco\Entrust\EntrustServiceProvider::class
 
     ],
 
@@ -228,6 +227,10 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        'JWTAuth'   => Tymon\JWTAuth\Facades\JWTAuth::class,
+        'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class,
+        'Entrust' => Zizaco\Entrust\EntrustServiceProvider::class
 
     ],
 
