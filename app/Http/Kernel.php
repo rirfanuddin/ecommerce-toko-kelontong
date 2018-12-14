3e5b03f6de27da27b1df31c05adf5559ce9f1094
Muhammad Rohman Irfanuddin <muhammad.rohman.irfanuddin09@gmail.com>
2018-12-14 15:46:45 +0700

    add basic auth middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -60,7 +60,8 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'ability' => 'App\Http\Middleware\TokenEntrustAbility'
+        'ability' => 'App\Http\Middleware\TokenEntrustAbility',
+        'basicAuth' => \App\Http\Middleware\BasicAuth::class
     ];
 
     /**
